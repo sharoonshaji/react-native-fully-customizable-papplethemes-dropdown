8cc8192f0ec6c073824511048d5c9ec3dd3d03cb
Experion <sharoon.shaji@experionglobal.com>
2020-08-01 16:46:21 +0530

    PappleDropdown

---
+++ index.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import {
+    Text, View, TouchableOpacity, StyleSheet, Image,
+    FlatList, TouchableWithoutFeedback, Modal, Dimensions,
+} from 'react-native'
+import PropTypes from 'prop-types'
+
+const { width, height } = Dimensions.get('window')
+const Images = { dropDownArrow: require('./Images/dropDownArrow') }
+const Metrics = {
+    screenWidth: width < height ? width : height,
+    screenHeight: width < height ? height : width,
+}
+
+class PappleDropDown extends React.Component {
+
+    static propTypes = {
+        showDropDown: PropTypes.bool,
+        selectedValue: PropTypes.string,
+        openCloseDropDown: PropTypes.func,
+        buttonContainerStyle: PropTypes.StyleSheet,
+        dropDownContainerStyle: PropTypes.StyleSheet,
+        dropDownData: PropTypes.array,
+        singleCellStyle: PropTypes.StyleSheet,
+        textStyle: PropTypes.StyleSheet,
+        onSelectItem: PropTypes.func,
+    }
+
+
+
+    renderSeparatorView = () => {
+        return (
+            <View style={styles.seperator} />
+        );
+    };
+
+
+
+
+    dropDownList(rowDatas: object, sectionID: number, rowID: number) {
+        const { singleCellStyle, textStyle, onSelectItem } = this.props
+        var rowData = rowDatas.item;
+        return (
+            <View style={[styles.dDListMainContainer, singleCellStyle]}>
+                <TouchableOpacity style={styles.dDListInnerContainer}
+                    onPress={onSelectItem.bind(this, rowData)}>
+                    <Text style={(textStyle) ? textStyle : styles.textStyle}>{rowData}</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+
+
+
+    chooseDropDownModal() {
+        const { showDropDown, openCloseDropDown,
+            dropDownContainerStyle, dropDownData } = this.props
+        return (
+            <Modal
+                animationType='fade'
+                transparent={true}
+                visible={showDropDown}
+            >
+                <TouchableWithoutFeedback onPress={() => openCloseDropDown()}>
+                    <View style={styles.dDModalOverlay} />
+                </TouchableWithoutFeedback>
+                <View style={styles.dDModalContent}>
+                    <View style={[styles.dDModalContainer, dropDownContainerStyle]}>
+                        <FlatList
+                            ref="dropDown"
+                            showsHorizontalScrollIndicator={false}
+                            data={dropDownData}
+                            renderItem={this.dropDownList.bind(this)}
+                            ItemSeparatorComponent={this.renderSeparatorView}
+                        />
+                    </View>
+                </View>
+            </Modal>
+        )
+    }
+
+
+
+    render() {
+        const { selectedValue, buttonContainerStyle, openCloseDropDown } = this.props
+        return (
+            <TouchableOpacity style={[styles.dDMainContainer, buttonContainerStyle]}
+                onPress={() => openCloseDropDown()}>
+                <Text style={styles.dropdownText}>{selectedValue}</Text>
+                <Image source={Images.dropDownArrow} style={styles.dropdownArrow} />
+                {this.chooseDropDownModal()}
+            </TouchableOpacity>
+        )
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+    dDMainContainer: {
+        height: Metrics.screenHeight / 15,
+        width: Metrics.screenWidth / 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+    },
+    dropdownText: {
+        color: 'black',
+        fontFamily: 'Montserrat-Regular',
+        fontSize: Metrics.screenWidth / 28,
+    },
+    dropdownArrow: {
+        height: Metrics.screenWidth / 28,
+        width: Metrics.screenWidth / 28,
+        left: Metrics.screenWidth / 50,
+    },
+    dDModalOverlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0,0,0,0.1)'
+    },
+    dDModalContent: {
+        flex: 1,
+    },
+    dDModalContainer: {
+        height: Metrics.screenHeight / 1.9,
+        width: Metrics.screenWidth / 2.02,
+        top: Metrics.screenHeight / 3.74,
+        backgroundColor: 'white',
+        zIndex: 10,
+        shadowColor: 'white',
+        shadowOpacity: 1,
+        elevation: 10,
+        shadowRadius: 20,
+        shadowOffset: { width: 5, height: 18 },
+        borderBottomLeftRadius: Metrics.screenWidth / 40,
+        borderBottomRightRadius: Metrics.screenWidth / 40,
+    },
+    seperator: {
+        height: 1,
+        width: '100%',
+        backgroundColor: "#CEDCCE",
+    },
+    dDListMainContainer: {
+        height: Metrics.screenHeight / 11.5,
+        width: Metrics.screenWidth / 2.02,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    dDListInnerContainer: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textStyle: {
+        color: 'black',
+        fontFamily: 'Montserrat-Regular',
+        fontSize: Metrics.screenWidth / 28,
+    },
+})
+
+export default PappleDropDown;
+
+
